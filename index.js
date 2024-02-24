fe0873631ad5e8372271c9518d0b625f6f428ec2
alisonkwork <156113052+alisonkwork@users.noreply.github.com>
2024-02-24 21:23:06 +0000

    Customize scroll reveal options

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-window.onload = function () {
-    ScrollReveal().reveal('.photos', {delay: 1000});
-}
+ScrollReveal().reveal('.photo1', {delay: 500, origin:'top', distance: '20px'});
+ScrollReveal().reveal('.photo2', {delay: 500, origin: 'right', distance:'50px'});
+ScrollReveal().reveal('.photo3', {delay: 500, origin: 'left', distance:'20px'});
+ScrollReveal().reveal('.photo4', {delay: 500, origin: 'right'});
+ScrollReveal().reveal('.photo5', {delay: 500, distance:'100px'});
+
 
