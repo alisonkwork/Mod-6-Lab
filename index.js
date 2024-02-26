2fd5d11e37c115555557acc09958c17babd75fd2
alisonkwork <156113052+alisonkwork@users.noreply.github.com>
2024-02-26 15:54:41 +0000

    Customize scroll reveal options, styling header and background

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-ScrollReveal().reveal('.photo1', {delay: 500, origin:'top', distance: '20px'});
-ScrollReveal().reveal('.photo2', {delay: 500, origin: 'right', distance:'50px'});
-ScrollReveal().reveal('.photo3', {delay: 500, origin: 'left', distance:'20px'});
-ScrollReveal().reveal('.photo4', {delay: 500, origin: 'right'});
-ScrollReveal().reveal('.photo5', {delay: 500, distance:'100px'});
+ScrollReveal().reveal('.photo1', {delay: 300, origin:'top', distance: '20px', duration: 1000});
+ScrollReveal().reveal('.photo2', {delay: 300, origin: 'left', distance:'50px', easing: 'cubic-bezier(.3, 0, 0, 3)', duration: 1500});
+ScrollReveal().reveal('.photo3', {delay: 300, origin: 'top', distance: '10px', duration: 1500});
+ScrollReveal().reveal('.photo4', {delay: 300, origin: 'right', distance: '80px', easing: 'cubic-bezier(.5, 1, 0, 2)', duration: 1200});
+ScrollReveal().reveal('.photo5', {delay: 300, distance:'100px', duration: 1200});
 
 
